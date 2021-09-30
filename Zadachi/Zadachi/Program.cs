bbe1243d6b53480a52bd87126e88886091ddf3d2
KariGrigorova <k.grigorova2017@pmg-pernik.com>
2021-09-30 13:15:30 +0300

    ff

---
+++ Zadachi/Zadachi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
